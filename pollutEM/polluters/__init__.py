3bfcd158492111cee4b842b6338ea6f87c281b6f
dkuska <70569959+dkuska@users.noreply.github.com>
2024-12-05 12:33:30 +0100

    Add ScalingShiftingPolluter

--- pollutEM/polluters/__init__.py
+++ pollutEM/polluters/__init__.py
@@ -1,14 +1,12 @@
 from .error_polluter import GaussianNoisePolluter, UniformNoisePolluter
-from .mathematical_polluter import (
-    ScalingPolluter,
-    ShiftingPolluter,
-)
+from .mathematical_polluter import ScalingPolluter, ShiftingPolluter, ScalingShiftingPolluter
 from .precision_polluter import RoundingPolluter
 
 __all__ = [
     "GaussianNoisePolluter",
     "RoundingPolluter",
     "ScalingPolluter",
+    "ScalingShiftingPolluter",
     "ShiftingPolluter",
     "UniformNoisePolluter",
 ]
@@ -17,13 +15,15 @@ __all__ = [
 def get_polluter(polluter_name, **kwargs):
     if polluter_name == "GaussianNoise":
         return GaussianNoisePolluter(**kwargs)
-    elif polluter_name == "RoundingPolluter":
+    elif polluter_name == "Rounding":
         return RoundingPolluter(**kwargs)
-    elif polluter_name == "ScalingPolluter":
+    elif polluter_name == "Scaling":
         return ScalingPolluter(**kwargs)
-    elif polluter_name == "ShiftingPolluter":
+    elif polluter_name == "ScalingShifting":
+        return ScalingShiftingPolluter(**kwargs)
+    elif polluter_name == "Shifting":
         return ShiftingPolluter(**kwargs)
-    elif polluter_name == "UniformNoisePolluter":
+    elif polluter_name == "UniformNoise":
         return UniformNoisePolluter(**kwargs)
     else:
         raise ValueError(f"No Polluter of Name {polluter_name} known")
--- pollutEM/polluters/mathematical_polluter.py
+++ pollutEM/polluters/mathematical_polluter.py
@@ -65,3 +65,36 @@ class ShiftingPolluter(BasePolluter):
 
     def _get_allowed_levels(self) -> list[str]:
         return ["cell", "row", "column"]
+
+
+class ScalingShiftingPolluter(BasePolluter):
+    def __init__(
+        self,
+        scale_amount: Union[int, float] = 1.0,
+        shift_amount: Union[int, float] = 1.0,
+        level: str = "column",
+        probability: Optional[float] = None,
+        indices: Optional[Union[list[int], list[str]]] = None,
+        seed: Optional[int] = None,
+    ):
+        self.shift_amount = shift_amount
+        self.scale_amount = scale_amount
+        super().__init__(
+            transformation=self.apply_shift,
+            level=level,
+            probability=probability,
+            indices=indices,
+            seed=seed,
+        )
+
+    def apply_shift(self, value):
+        return (value * self.scale_amount) + self.shift_amount
+
+    def _get_type_mapping(self) -> dict:
+        return {
+            np.float64: np.float64,
+            np.int64: np.int64,
+        }
+
+    def _get_allowed_levels(self) -> list[str]:
+        return ["cell", "row", "column"]
