09843877f1f3bdba365fdec6b53acc7cad7f3a6e
dkuska <70569959+dkuska@users.noreply.github.com>
2025-01-13 19:51:48 +0100

    Extend earthquakes master config

---
+++ scripts.sh
@@ -0,0 +1,91 @@
+# Earthquakes
+python main.py \
+    --dataset_path /home/nano/projects/pollutEM/data/earthquakes/features.csv\
+    --master_config_path /home/nano/projects/pollutEM/config/earthquakes_master.yaml \
+    --train-split /home/nano/projects/pollutEM/data/earthquakes/embitto_train_0_goldstandard.csv \
+    --validation-split /home/nano/projects/pollutEM/data/earthquakes/embitto_valid_0_goldstandard.csv \
+    --test-split /home/nano/projects/pollutEM/data/earthquakes/embitto_test_0_goldstandard.csv \
+    --output_dir /home/nano/projects/pollutEM/reports \
+    --samples_per_size 5
+
+python main.py \
+    --dataset_path /home/nano/projects/pollutEM/data/earthquakes/features.csv\
+    --master_config_path /home/nano/projects/pollutEM/config/earthquakes_master.yaml \
+    --train-split /home/nano/projects/pollutEM/data/earthquakes/embitto_train_1_goldstandard.csv \
+    --validation-split /home/nano/projects/pollutEM/data/earthquakes/embitto_valid_1_goldstandard.csv \
+    --test-split /home/nano/projects/pollutEM/data/earthquakes/embitto_test_1_goldstandard.csv \
+    --output_dir /home/nano/projects/pollutEM/reports \
+    --samples_per_size 5
+
+python main.py \
+    --dataset_path /home/nano/projects/pollutEM/data/earthquakes/features.csv\
+    --master_config_path /home/nano/projects/pollutEM/config/earthquakes_master.yaml \
+    --train-split /home/nano/projects/pollutEM/data/earthquakes/embitto_train_2_goldstandard.csv \
+    --validation-split /home/nano/projects/pollutEM/data/earthquakes/embitto_valid_2_goldstandard.csv \
+    --test-split /home/nano/projects/pollutEM/data/earthquakes/embitto_test_2_goldstandard.csv \
+    --output_dir /home/nano/projects/pollutEM/reports \
+    --samples_per_size 5
+
+python main.py \
+    --dataset_path /home/nano/projects/pollutEM/data/earthquakes/features.csv\
+    --master_config_path /home/nano/projects/pollutEM/config/earthquakes_master.yaml \
+    --train-split /home/nano/projects/pollutEM/data/earthquakes/embitto_train_3_goldstandard.csv \
+    --validation-split /home/nano/projects/pollutEM/data/earthquakes/embitto_valid_3_goldstandard.csv \
+    --test-split /home/nano/projects/pollutEM/data/earthquakes/embitto_test_3_goldstandard.csv \
+    --output_dir /home/nano/projects/pollutEM/reports \
+    --samples_per_size 5
+
+python main.py \
+    --dataset_path /home/nano/projects/pollutEM/data/earthquakes/features.csv\
+    --master_config_path /home/nano/projects/pollutEM/config/earthquakes_master.yaml \
+    --train-split /home/nano/projects/pollutEM/data/earthquakes/embitto_train_4_goldstandard.csv \
+    --validation-split /home/nano/projects/pollutEM/data/earthquakes/embitto_valid_4_goldstandard.csv \
+    --test-split /home/nano/projects/pollutEM/data/earthquakes/embitto_test_4_goldstandard.csv \
+    --output_dir /home/nano/projects/pollutEM/reports \
+    --samples_per_size 5
+
+# Laptops
+python main.py \
+    --dataset_path /home/nano/projects/pollutEM/data/laptops/features.csv\
+    --master_config_path /home/nano/projects/pollutEM/config/laptops_master.yaml \
+    --train-split /home/nano/projects/pollutEM/data/laptops/embitto_train_0_goldstandard.csv \
+    --validation-split /home/nano/projects/pollutEM/data/laptops/embitto_valid_0_goldstandard.csv \
+    --test-split /home/nano/projects/pollutEM/data/laptops/embitto_test_0_goldstandard.csv \
+    --output_dir /home/nano/projects/pollutEM/reports \
+    --samples_per_size 5
+
+python main.py \
+    --dataset_path /home/nano/projects/pollutEM/data/laptops/features.csv\
+    --master_config_path /home/nano/projects/pollutEM/config/laptops_master.yaml \
+    --train-split /home/nano/projects/pollutEM/data/laptops/embitto_train_1_goldstandard.csv \
+    --validation-split /home/nano/projects/pollutEM/data/laptops/embitto_valid_1_goldstandard.csv \
+    --test-split /home/nano/projects/pollutEM/data/laptops/embitto_test_1_goldstandard.csv \
+    --output_dir /home/nano/projects/pollutEM/reports \
+    --samples_per_size 5
+
+python main.py \
+    --dataset_path /home/nano/projects/pollutEM/data/laptops/features.csv\
+    --master_config_path /home/nano/projects/pollutEM/config/laptops_master.yaml \
+    --train-split /home/nano/projects/pollutEM/data/laptops/embitto_train_2_goldstandard.csv \
+    --validation-split /home/nano/projects/pollutEM/data/laptops/embitto_valid_2_goldstandard.csv \
+    --test-split /home/nano/projects/pollutEM/data/laptops/embitto_test_2_goldstandard.csv \
+    --output_dir /home/nano/projects/pollutEM/reports \
+    --samples_per_size 5
+
+python main.py \
+    --dataset_path /home/nano/projects/pollutEM/data/laptops/features.csv\
+    --master_config_path /home/nano/projects/pollutEM/config/laptops_master.yaml \
+    --train-split /home/nano/projects/pollutEM/data/laptops/embitto_train_2_goldstandard.csv \
+    --validation-split /home/nano/projects/pollutEM/data/laptops/embitto_valid_2_goldstandard.csv \
+    --test-split /home/nano/projects/pollutEM/data/laptops/embitto_test_2_goldstandard.csv \
+    --output_dir /home/nano/projects/pollutEM/reports \
+    --samples_per_size 5
+
+python main.py \
+    --dataset_path /home/nano/projects/pollutEM/data/laptops/features.csv\
+    --master_config_path /home/nano/projects/pollutEM/config/laptops_master.yaml \
+    --train-split /home/nano/projects/pollutEM/data/laptops/embitto_train_2_goldstandard.csv \
+    --validation-split /home/nano/projects/pollutEM/data/laptops/embitto_valid_2_goldstandard.csv \
+    --test-split /home/nano/projects/pollutEM/data/laptops/embitto_test_2_goldstandard.csv \
+    --output_dir /home/nano/projects/pollutEM/reports \
+    --samples_per_size 5
